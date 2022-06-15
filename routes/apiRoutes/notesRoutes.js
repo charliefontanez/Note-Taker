11fa2c0b1d138787057e8c87c4461db564f1f81f
Carlos Fontanez <charliefontanezis@gmail.com>
2022-06-15 00:14:02 -0400

    added ability to delete notes

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,33 +1,19 @@
 const router = require('express').Router();
-// const { createNewNote } = require('../../lib/noteFunctions');
+const { createNewNote, deleteNote } = require('../../lib/noteFunctions');
 const notes = require('../../db/notes');
-const { v4: uuidv4 } = require('uuid');
-const fs = require('fs');
-const path = require('path');
-
-function createNewNote(body, notes) {
-  body.id = uuidv4();
-  const note = body;
-  console.log(body);
-  notes.push(note);
-  fs.writeFileSync(
-    path.join(__dirname, '../../db/notes.json'), 
-    JSON.stringify(notes, null, 2)
-  );
-  return note;
-}
-
-function deleteNote() {}
-
 
 router.get('/notes', (req, res) => {
   let results = notes;
-  console.log(results);
   res.json(notes);
 });
 
 router.post('/notes', (req, res) => {
   const note = createNewNote(req.body, notes);
+  res.json(note);
+});
+
+router.delete('/notes/:id', (req, res) => {
+  const note = deleteNote(req.params.id, notes);
   res.json(note);
 })
 
